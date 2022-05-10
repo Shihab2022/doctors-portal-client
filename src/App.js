686511fb32f1e57be3368b3d3d98ae237ff0dbd8
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-10 11:44:33 +0600

    start doctors portal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import LoginPage from './Pages/Login/LoginPage';
+import Navbar from './Pages/Sherad/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <Navbar></Navbar>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/home" element={<Home></Home>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
+      <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/" element={<Home></Home>}></Route>
+    </Routes>
     </div>
   );
 }
