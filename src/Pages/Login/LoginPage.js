42ba899e50468c66e7f099c717eb35fef1f86880
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-12 12:36:14 +0600

    sign in with google and implement react hook form

--- src/Pages/Login/LoginPage.js
+++ src/Pages/Login/LoginPage.js
@@ -1,44 +1,90 @@
 // import { Input } from 'postcss';
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+import Spinner from "../Sherad/Spinner";
 const LoginPage = () => {
   const { register, errors, handleSubmit } = useForm();
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+if(loading){
+    return <Spinner></Spinner>
+}
+if(error){
+    console.log(error)
+}
   const onSubmit = (data) => {
-    console.log(data);
+    console.log(data.email);
   };
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center h-screen items-center">
         <div class="card shadow-2xl w-1/4  text-neutral-content">
   <div class="card-body ">
-   
+   <h1 className="text-3xl font-bold text-center uppercase text-primary">login </h1>
   <form onSubmit={handleSubmit(onSubmit)}>
         <label className="label">
           <span className="label-text">Email</span>
         </label>
         <input
-          name="email"
-          {...register("email", {
-            pattern: {
-              value: /[A-Za-z]{3}/,
-              message: 'error message' // JS only: <p>error message</p> TS only support string
-            }
-          })}
+         
           type="email"
           placeholder="Your Email"
-          className="input input-bordered w-full "
+          className="input input-bordered w-full max-w-xs "
+          {...register("email", {
+              required: {
+                value: true,
+                message: 'Email address is required'
+              },
+            pattern: {
+              value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+              message: 'Your email is valid .' // JS only: <p>error message</p> TS only support string
+            }
+          })}
+          
         />
         <label className="label">
-          <span className="label-text-alt">Alt label</span>
+           
+        {errors?.email?.type==='required' && <span className="text-red-500 label-text-alt">{errors.email.message}</span>}
+        {errors?.email?.type==='pattern' && <span className="text-red-500 label-text-alt">{errors.email.message}</span>}
         </label>
 
-        {errors?.email && "First name is required"}
-        {/* <Input name="lastName" ref={register({ required: true })} />
-      {errors.lastName && "Last name is required"} */}
+
+        <label className="label">
+          <span className="label-text">Password</span>
+        </label>
+        <input
+         
+          type="password"
+          placeholder="Your Password"
+          className="input input-bordered w-full max-w-xs "
+          {...register("password", {
+              required: {
+                value: true,
+                message: 'Password address is required'
+              },
+            pattern: {
+              value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+              message: 'Your password is valid .' // JS only: <p>error message</p> TS only support string
+            }
+          })}
+          
+        />
+        <label className="label">
+           
+        {errors?.password?.type==='required' && <span className="text-red-500 label-text-alt">{errors.password.message}</span>}
+        {errors?.password?.type==='pattern' && <span className="text-red-500 label-text-alt">{errors.password.message}</span>}
+        </label>
+
+    
         <input className="btn btn-accent text-center w-full" type="submit" />
       </form>
+
+      <div class="divider">OR</div>
+      <button onClick={() =>signInWithGoogle()} className='w-full btn btn-outline btn-accent'>Continue with google</button>
   </div>
 </div>
-    
+
     </div>
   );
 };
---
+++ src/Pages/Sherad/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div>
+            
+
+<div class="flex items-center h-screen justify-center ">
+    <div class="w-52 h-52 border-t-4 border-b-4 border-pink-900 rounded-full animate-spin"></div>
+</div>
+        </div>
+    );
+};
+
+export default Spinner;
